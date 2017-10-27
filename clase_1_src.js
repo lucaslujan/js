d322e2fb53ce6a826a5d950cd0f4fc46bd484916
lucaslujan <31803359+lucaslujan@users.noreply.github.com>
2017-10-27 15:59:37 -0300

    27-10

--- clase_1_src.js
+++ clase_1_src.js
@@ -1,28 +1,40 @@
-
 // var boleano = true
 
 // var nombre = 'Pedro'
 
-// var numero = 2
+//var numero = 2
 
 // var nulleano = null
 
 // var pedrito = [1, 'Juan', false, null, [], 3, 4, 'Clara', {}] //
 
+// var persona = {
+//   nombre: 'Laura',
+//   apellido: 'Lopez',
+//   edad: 26,
+//   mayorDeEdad: true,
+//   hijos: [
+//     {
+//       nombre: 'Clara',
+//       apellido: 'Fernandez'
+//     },
+//     {
+//       nombre: 'Pedrito',
+//       apellido: 'Fernandez'
+//     }
+//   ],
+//   direccion: {
+//     calle: 'Av. Gaona',
+//     altura: 1234
+//   }
+// }
 
-var persona = {nombre: 'Laura', apellido: 'Lopez', edad: 26, mayorDeEdad: true, hijos: [
-{nombre: 'Clara', apellido: 'Fernandez'},{nombre: 'Pedrito', apellido: 'Fernandez'}],
-direccion: {calle: 'Av. Gaona',altura: 1234}}
+// var hijos = persona.hijos
 
-var hijos = persona.hijos
+// console.log(persona.hijos[0].nombre)
 
-console.log(persona.hijos[0].nombre)
+//console.log('Hola ' + persona.nombre + ', ' + persona.apellido + ' tu edad es: ' + persona.edad)
 
-console.log('Hola ' + persona.nombre + ', ' + persona.apellido + ' tu edad es: ' + persona.edad)*/
-
-
-
-var numero
 
 numero = prompt('Ingrese un número:')
 
@@ -31,5 +43,3 @@ numeroParseado = parseInt(numero, 10)
 var suma = numero + numero
 
 alert('El numero elegido es: ' + suma)
-
-
--- ejercicio1.js
+++ ejercicio1.js
@@ -1,12 +1,12 @@
 // Pedir nombre mediante prompt y disparar un alert con algún texto más ->
 
-var firstName
-
-firstName = prompt('Escribí tu nombre:', 'Ej: Juan')
-
-var message1 = 'Tu nombre es: ' + firstName + ' !'
-
-alert(message1)
+//var firstName
+//
+//firstName = prompt('Escribí tu nombre:', 'Ej: Juan')
+//
+//var message1 = 'Tu nombre es: ' + firstName + ' !'
+//
+//alert(message1)
 
 // Pedir un número y sumarlo a otro que este en una variable ->
 
@@ -20,20 +20,20 @@ alert('La suma del número guardado y el que ingresaste es: ' + add1 + ' !')
 
 // Pedir un número y restarlo a otro que este en una variable ->
 
-var number3 = prompt('Escribí un número:', 'Ej: 8')
-
-var sub = number1 - parseFloat(number3, 10)
-
-alert('La resta del número guardado y el que ingresaste es: ' + sub + ' !')
-
-// Pedir un número, luego otro y ambos sumarlos ->
-
-var number4 = prompt('Escribí un número:', 'Ej: 8')
-
-var number5 = prompt('Escribí un número:', 'Ej: 8')
-
-var resultado = parseFloat(number4, 10) + parseFloat(number5, 10)
-
-alert('La suma de los dos números que ingresaste es: ' + resultado + ' !')
-
-alert('Completaste el ejercicio !')
+//var number3 = prompt('Escribí un número:', 'Ej: 8')
+//
+//var sub = number1 - parseFloat(number3, 10)
+//
+//alert('La resta del número guardado y el que ingresaste es: ' + sub + ' !')
+//
+//// Pedir un número, luego otro y ambos sumarlos ->
+//
+//var number4 = prompt('Escribí un número:', 'Ej: 8')
+//
+//var number5 = prompt('Escribí un número:', 'Ej: 8')
+//
+//var resultado = parseFloat(number4, 10) + parseFloat(number5, 10)
+//
+//alert('La suma de los dos números que ingresaste es: ' + resultado + ' !')
+//
+//alert('Completaste el ejercicio !')
